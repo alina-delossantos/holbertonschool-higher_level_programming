79af2e66ec3101a38fb5c5424e90cdc3340b0366
root <alina.delossantos@gmail.com>
2022-01-03 01:56:15 +0000

    fjfjfj

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -3,7 +3,7 @@
 if __name__ == '__main__':
     from add_0 import add
 
-a = 1
-b = 2
+    a = 1
+    b = 2
 
-print('{:d} + {:d} = {:d}'.format(a,b, add(a,b)))
+    print('{:d} + {:d} = {:d}'.format(a,b, add(a,b)))
