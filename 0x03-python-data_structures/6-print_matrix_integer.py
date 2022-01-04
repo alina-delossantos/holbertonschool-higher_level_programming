149e479d3a4034aa1fdf679d439fc00bd58dcad7
root <alina.delossantos@gmail.com>
2022-01-04 19:05:35 +0000

    fhfh

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+def print_matrix_integer(matrix=[[]]):
+    if len(matrix[0]) != 0:
+        x = len(matrix)
+        y = len(matrix[0])
+        for item in range(x):
+            for subitem in range(y):
+                if subitem == y - 1:
+                    print('{:d}'.format(matrix[item][subitem]))
+                else:
+                    print('{:d}'.format(matrix[item][subitem]), end='')
+    else:
+        print()
