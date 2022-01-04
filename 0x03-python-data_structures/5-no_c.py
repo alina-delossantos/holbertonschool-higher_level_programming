4d198b5ba4edb27c883c83f0dc2f3f883255ecbc
root <alina.delossantos@gmail.com>
2022-01-04 02:29:51 +0000

    fjfjf

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,8 +2,5 @@
 
 
 def no_c(my_string):
-    if my_string:
-        not_allowed = "cC"
-        for letter in not_allowed:
-            new_string = my_string.replace(letter, "")
-        return(new_string)
+    new_string = my_string.translate({ord(c): None for c in "cC"})
+    return(new_string)
