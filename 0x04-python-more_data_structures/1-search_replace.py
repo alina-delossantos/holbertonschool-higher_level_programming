8a50f4d384ac96b433eb0e1ede9b1998bcb65bf1
root <alina.delossantos@gmail.com>
2022-01-04 23:37:23 +0000

    fjfjjf

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+def search_replace(my_list, search, replace):
+    if my_list is not None:
+        replaced_list = []
+        for i, j in enumerate(my_list):
+            if my_list[i] = search:
+                replaced_list.append(replace)
+            else:
+                replaced_list.append(my_list[i])
+        return replaced_list
