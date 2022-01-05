40978e4eef42489db95bfeaf3cf5ed924f7502e4
root <alina.delossantos@gmail.com>
2022-01-05 00:30:20 +0000

    fjfj

---
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+
+def multiply_by_2(a_dictionary):
+    if a_dictionary is not None:
+        new_dictionary = a_dictionary.copy()
+        for i, j in new_dictionary.items():
+            new_dictionary[i] = j*2
+        return new_dictionary
