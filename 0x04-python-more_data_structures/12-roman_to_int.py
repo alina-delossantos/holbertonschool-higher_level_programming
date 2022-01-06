5e544f6a287770a25f27f36d3f70f14fc4d7498d
root <alina.delossantos@gmail.com>
2022-01-06 01:26:43 +0000

    fjfj

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,7 +2,6 @@
 
 
 def roman_to_int(roman_string):
-    if roman_string is not None and type(roman_string) == str:
         r_t = {
             "I": 1,
             "V": 5,
@@ -12,6 +11,9 @@ def roman_to_int(roman_string):
             "D": 500,
             "M": 1000
         }
+
+        if roman_string is None or type(roman_string) != str:
+            return 0
 
         decimal = 0
 
