3c24890f72a36e9ea13f89d6961ce92001946fa1
root <alina.delossantos@gmail.com>
2022-01-23 23:00:33 +0000

    pep8

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -5,11 +5,10 @@ Adds two int (a, b) and returns its sum
 Float is converted to int, otherwise raise TypeError
 """
 
+
 def add_integer(a, b=98):
 
-    """ add_integer - adds two integers (a, b)
-    Returns: integer sum
-    """
+    """Returns: integer sum"""
 
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
