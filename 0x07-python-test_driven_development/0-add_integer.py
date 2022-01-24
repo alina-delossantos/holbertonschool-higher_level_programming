d3db9188d94abc1fc1c8a28e069af76525fd183f
root <alina.delossantos@gmail.com>
2022-01-24 19:03:01 +0000

    replaced isinstance by type

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -10,13 +10,9 @@ def add_integer(a, b=98):
 
     """Returns: integer sum"""
 
-    if not isinstance(a, (int, float)):
+    if type(a) is not int and type(a) is not float:
         raise TypeError("a must be an integer")
-    else:
-        a = int(a)
-    if not isinstance(b, (int, float)):
+    if type(b) is not int and type(b) is not float:
         raise TypeError("b must be an integer")
-    else:
-        b = int(b)
 
-    return a + b
+    return int(a + b)
