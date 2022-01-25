8638fd99e13259abe1a4f88ff5d7804dd3b565a8
root <alina.delossantos@gmail.com>
2022-01-25 00:38:12 +0000

    fjjf

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
+#!/usr/bin/python3
+"""
+    Module contains of a single class
+"""
 
 
 class Rectangle:
-    """Rectangle class."""
+    """Defines a reactangle"""
     pass
-
-
-if __name__ == '__main__':
-    my_rectangle = Rectangle()
-    print(type(my_rectangle))
-    print(my_rectangle.__dict__)
