2026b03fbe9fa841b30891d9c10edd6af6675f86
root <alina.delossantos@gmail.com>
2022-01-28 02:04:03 +0000

    fhfh

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -8,9 +8,9 @@ It provides the print_square fx
 
 def print_square(size):
     """prints a square with # of size n"""
-    if size < 0:
-        raise ValueError("size must be >= 0")
     if type(size) is not int:
         raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
     if size > 0:
         print(("#" * size + "\n") * size, end="")
