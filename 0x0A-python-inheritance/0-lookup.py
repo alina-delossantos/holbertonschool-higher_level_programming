0abb221d2a02b8b46b65cd132b8cc30d6b192fdf
root <alina.delossantos@gmail.com>
2022-01-31 23:08:36 +0000

    ffhf

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 
 
-#!/usr/bin/python3
 """Lookup module
 function that returns the list of available attributes and methods of an object
 """
 
 
 def lookup(object):
-    """function that returns the list of available attributes and methods of an object"""
+    """function that returns the list of available attributes of obj"""
     return dir(object)
