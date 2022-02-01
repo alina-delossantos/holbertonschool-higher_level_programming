5ed202e3bf167b45e5b93f48e0edf3d36b823b3c
root <alina.delossantos@gmail.com>
2022-02-01 02:34:47 +0000

    fhfh

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -10,13 +10,3 @@ def is_kind_of_class(obj, a_class):
     if isinstance(obj, a_class):
         return True
     return False
-
-
-if __name__ == '__main__':
-    a = 1
-    if is_kind_of_class(a, int):
-        print("{} comes from {}".format(a, int.__name__))
-    if is_kind_of_class(a, float):
-        print("{} comes from {}".format(a, float.__name__))
-    if is_kind_of_class(a, object):
-        print("{} comes from {}".format(a, object.__name__))
