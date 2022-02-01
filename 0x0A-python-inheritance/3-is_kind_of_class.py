89a1da8808ee8c8162ecb8ca1b0d03a6268850cb
root <alina.delossantos@gmail.com>
2022-02-01 02:42:02 +0000

    jfjf

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Module cks if object is instance of certain class"""
 
+
 def is_kind_of_class(obj, a_class):
     """True if obj of same instance as class or False
     Args:
